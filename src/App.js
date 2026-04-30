fc1104f53ba4bb85a342d85fa8b87c7a4c65099f
STHEFANNY DE ALMEIDA BRANDÃO <sthefanny_brandao@estudante.sesisenai.org.br>
2026-04-30 14:53:11 -0300

    cartas de pokemon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [pokemons, setPokemons] = useState({})
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await
+          axios.get('https://pokeapi.co/api/v2/pokemon/fennekin');
+        setPokemons(res.data);
+        console.log('success:', res.data);
+        setLoading(false);
+
+      }
+      catch (err) {
+        console.error("erro ao carregar API", err);
+        setLoading(false)
+        setError(true)
+      }
+    };
+    getData();
+  }, {});
+
+
+  if (loading) return (
+    <div className="loader">
+      carregando pokédex...
+  </div>
+  );
+
+  if (error) return (
+    <div className="error">
+      ocorreu um erro inesperado
+  </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app"> 
+       <h1> poke card</h1>
+       <div className="pokemon-container"> 
+
+       <div className="pokemon-card">
+       <h3>{pokemons.name}</h3>
+       <h3>{pokemons.tipo}</h3>
+       <img src={pokemons.sprites.front_shiny} alt={pokemons.name}/>
+       <h3>{pokemons.stats[0].stat.name + ''+
+     pokemons.stats[0].base_stat + pokemons.stats[1].stat.name + '' +
+     pokemons.stats[0].base_stat}</h3>
+        </div>
+        </div>
+        </div>
   );
 }
 

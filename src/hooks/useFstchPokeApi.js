678c5ee5adb7af2c2635caeb18af5766e7b5538d
STHEFANNY DE ALMEIDA BRANDÃO <sthefanny_brandao@estudante.sesisenai.org.br>
2026-06-11 15:57:22 -0300

    fucionando

--- src/hooks/useFstchPokeApi.js
+++ src/hooks/useFstchPokeApi.js
@@ -7,19 +7,17 @@ function useFetchPokeapi(pokemon) {
   const [pokemons, setPokemons] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [specie, setSpecie] = useState({});
+  const { evolution, setEvolution } = useState({});
+  const { myPokemon, setMyPokemon } = useState({});
 
 
+  //get inicio//
   useEffect(() => {
     const getData = async () => {
       try {
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`,
-          {
-            headers: {
-              'Cache-Control': 'no-cache',
-              'Pragma': 'no-cache',
-              'Expires': '0',
-            }
-          });
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`// await aguardar o axios trezer informacoes do axios
+        );
 
         setPokemons(res.data);
         console.log('Success:', res.data);
@@ -33,7 +31,66 @@ function useFetchPokeapi(pokemon) {
     };
     getData();
   }, [pokemon]);
-  return { pokemons, loading, error }
+  //get da especie//
+  useEffect(() => {
+    const getSpecies = async () => {
+      try {
+        console.log('Success: teste 1', pokemons);
+
+        const res = await axios.get(pokemons.species.url, {})
+
+        setSpecie(res.data);
+        console.log('Success:', res.data);
+
+      }
+      catch (err) {
+        console.error("Erro ao carregar API", err);
+
+      }
+    };
+    getSpecies();
+  }, [pokemons]);
+
+
+
+  useEffect(() => {
+    const getEvolutions = async () => {
+      try {
+        const res = await axios.get(specie.evolution_chain.url, {})// await aguardar o axios trezer informacoes do axios
+
+
+        setEvolution(res.data);
+        console.log('Success:', res.data);
+
+      }
+      catch (err) {
+        console.error("Erro ao carregar API", err);
+
+      }
+    };
+    getEvolutions();
+  }, [specie]);
+
+  useEffect(() => {
+    const setPoke = async () => {
+      try {
+        setMyPokemon({
+          nome: pokemons.name,
+          vida: pokemons.stats[0].base_stat,
+          ataque: pokemons.stats[1].base_stat,
+          tipo: pokemons.types[0].type.name,
+          evolucao: [evolution.chain.evolves_to[0].species.name],
+          imagem: pokemons.sprites.front_default,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    setPoke();
+  }, [evolution, pokemons]);
+  console.log('Success: teste');
+  return { myPokemon, loading, error }
 }
 
 export default useFetchPokeapi;
+
--- src/pages/carderShuppet/cardershu.js
+++ src/pages/carderShuppet/cardershu.js
@@ -10,10 +10,9 @@ import axios from 'axios';
 
 function CarderShuppet() {
   const { id } = useParams();// ele pegar informaçao da url 
-  const {pokemons, loading, error} = useFetchPokeapi(id); //pegar as informacoes do pokemon que vc ta utilizando no API, API: e tipo uma ponte 
+  const {myPokemon, loading, error} = useFetchPokeapi(id); //pegar as informacoes do pokemon que vc ta utilizando no API, API: e tipo uma ponte 
   //que permite que dois aplicativos ou sistemas diferentes conversem entre si. ex://
-  const[poke,setPoke] = useState({})
-
+   
   
   
   if (loading) return (
@@ -28,39 +27,27 @@ function CarderShuppet() {
   </div>
   );
 
-  if(pokemons){
-    setPoke(
-      {
-        "nome":pokemons.name,
-        "vida":pokemons.stats,[0].base_stat,
-        "ataque":pokemons.stats,[1].base_stat,
-        "tipo":pokemons.types,[0]
-        ""
-        
-      }
-    )
-  }
+
+    
+  
    
   return (
      
-    <div className={`app3 ${pokemons.types[0].type.name}`}> 
-       <div className={`pokemon-container3 ${pokemons.types[0].type.name}`}> 
+    <div className={`app3 ${myPokemon.nome}`}> 
+       <div className={`pokemon-container3 ${myPokemon.nome}`}> 
 
-       <div className={`pokemon-card3 ${pokemons.types[0].type.name}`}>
+       <div className={`pokemon-card3 ${myPokemon.nome}`}>
 
     
         
-       <h2>{pokemons.name}</h2>
+       <h2>{myPokemon.nome}</h2>
         
-       <img src={pokemons.sprites.front_default } alt={pokemons.name}/>
+       <img src={myPokemon.imagem} alt={myPokemon.nome}/>
 
-       <h3>{pokemons.stats[0].stat.name + ''+
+       <h3>{myPokemon.ataque+ ''+
 
-     pokemons.stats[0].base_stat + pokemons.stats[1].stat.name + '' +
-
-     pokemons.stats[0].base_stat +
-     
-     pokemons.types[0].type.name }</h3>
+     myPokemon.vida + '' +
+     myPokemon.tipo }</h3>
       
         </div>
         </div>
